7cc91486246e82cbc7847707af60068935ed01c9
Cesar Humberto Grifaldo Garcia <cesar.grifaldo@elektra.com.mx>
2023-09-26 14:39:14 -0600

    Initial Commit

---
+++ ViperArchitectureNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViperArchitectureNews
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
